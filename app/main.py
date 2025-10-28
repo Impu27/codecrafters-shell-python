1177f7f08053a2059d422579e0cafbac30328f51
Impu27 <impudayanand2002@gmail.com>
2025-10-28 23:08:31 +0530

    Code to implement REPL

--- app/main.py
+++ app/main.py
@@ -2,11 +2,27 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    # Captures the user's command in the "command" variable
-    command = input()
-    print(f"{command}: command not found")
-    pass
+    while True:
+        # READ
+        sys.stdout.write("$ ")
+        sys.stdout.flush() # ensures the prompt appears immediately
+        try:
+            #wait for user input
+            command = input().strip()
+        except EOFError:
+            # When user presses Ctrl+D, gracefully exit
+            break
+        except KeyboardInterrupt:
+            # Handle Ctrl+C gracefully
+            sys.stdout.write("\n")
+            continue
+        # EVAL
+        if command == "":
+            # Empty input: just show prompt again
+            continue
+        # PRINT
+        print(f"{command}: command not found")
+    
 
 
 if __name__ == "__main__":
